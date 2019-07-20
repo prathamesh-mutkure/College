4c6d56a5bca0564ffa4967bd870ba3f8088aac25
Prathamesh Mutkure <pmutkure009@gmail.com>
2019-07-21 01:04:06 +0530

    Init CGR

---
+++ Sem-2/CGR-MP/cmake-build-debug/CMakeFiles/CGR_MP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CGR_MP.dir/main.cpp.o"
+  "CGR_MP.pdb"
+  "CGR_MP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CGR_MP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sem-2/CGR-MP/cmake-build-debug/CMakeFiles/CGR_MP.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CGR_MP.
+# This may be replaced when dependencies are built.
